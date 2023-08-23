ee0d21753eb91f1e9674bbb17e33d9d98369103e
sakurahayato <kawakamihakase@gmail.com>
2023-08-23 10:42:35 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import time
+st.title('Streamlit 超入門')
+
+st.write('プレグレスバーの表示')
+'Start!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration{i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+'Done!!!!'
+expander = st.expander('問い合わせ')
+expander.write('問い合わせ内容を書く')
+
+left_column,right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラム')
+#text = st.text_input('あなたの趣味を教えて下さい。')
+#'あなたの趣味は、:',text
+
+#condition = st.slider('あなたの今の調子は？', 0, 100, 50)
+
+#'コンディション:',condition
+
+
+#
+# st.table(df.style.highlight_max(axis=0))
+
